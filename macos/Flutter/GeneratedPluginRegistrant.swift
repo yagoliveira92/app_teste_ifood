c1451f1cdb24837f9afe834c14b57d9c74c19aef
Yago Souza Oliveira <yagoliveira92@gmail.com>
2024-11-19 11:01:56 -0300

    feat: Correção e ajustes para o Ladybug

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import flutter_secure_storage_macos
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
